6582b7bac546f699adab377692f3b054ebdbfd3e
Dara Lasisi <daralasisi@gmail.com>
2024-07-21 22:09:49 +0100

    updated code to print data on screen

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,14 +4,13 @@ import json
 
 api_key = 'ZAKSXJ3OE1BQFASN'
 symbol = 'GOOGL'
-url = f'https://www.alphavantage.co/query?
-function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}'
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}'
 
 def get_data_from_api(api_key, symbol, url):
     response = requests.get(url)
     data = response.json()
 
-    print (f' the type is: {type(data)}')
+    print (data)
     return data
 
 data = get_data_from_api(api_key, symbol, url)
