52ce24cafd81b4c7d7de0d06a7457de6ffa87256
Dara Lasisi <daralasisi@gmail.com>
2024-08-19 17:53:43 +0100

    started doing the db stuff

--- src/pipeline.py
+++ src/pipeline.py
@@ -8,10 +8,14 @@ symbol = 'GOOGL'
 def get_data_from_api(api_key, symbol):
 
     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}'
-    response = requests.get(url)
+    headers = {'Authorisation': f'Bearer {api_key}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status() 
     data = response.json()
+    print('Fetched data:', data['Meta Data'])
+    return data
 
-    if 'Time Series (Daily)' in data:
+    ''' if 'Time Series (Daily)' in data:
         df = pd.DataFrame(data['Time Series (Daily)']).transpose()
         df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
         df.index = pd.to_datetime(df.index)
@@ -22,7 +26,7 @@ def get_data_from_api(api_key, symbol):
     else:
         print (f'Error fetching data for {symbol}: {data}')
         return pd.DataFrame()
-
+    '''
 data = get_data_from_api(api_key, symbol)
 
 
